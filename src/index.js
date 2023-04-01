bce16f02f034436d01a68353780a0240124a9672
Isaac Wanger <teryima127@gmail.com>
2023-04-01 19:09:53 +0100

    props

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
+
 const BookList = () => {
-    return <section></section>
+    return <section className="booklist">
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/> 
+    </section>
 }
 
 const Book = () => {
-    return <article>
-
+    return <article className="book">
+        <Image/>
+        <Title/>
+        <Author/>
     </article>
-}
-const Image = () => <h2>image placeholder</h2>
-const Title = () => <h2>Book Title</h2>
+};
+const Image = () => (<img src="https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/714Da0RQA5L._AC_UL600_SR600,400_.jpg" alt="Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones"/>)
+const Title = () => <h2>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h2>
 const Author = () => {
-    return <h4>Author</h4>
+    return <h4 style={{color:"#617d98", fontSize:"0.75rem", marginTop:"0.5rem"}}>James Clear </h4>
 };
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting/>)
+root.render(<BookList/>)
 
 
 // Or use arrow function
